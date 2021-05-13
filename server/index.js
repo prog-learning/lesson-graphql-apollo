8c3ca8737829ae2dff8db83a6129719fc399a779
nbr41to <nbr.41to@gmail.com>
2021-05-13 10:48:49 +0900

    .

--- server/index.js
+++ server/index.js
@@ -1,28 +1,89 @@
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
+// const { graphqlHTTP } = require('express-graphql');
 const {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
 } = require('graphql');
 
-const app = express();
+const { ApolloServer } = require('apollo-server');
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'hello',
-    fields: () => ({
-      message: {
-        type: GraphQLString,
-        resolve: () => 'hello gql'
-      }
-    })
-  })
+// const app = express();
+
+// 型を定義
+/**
+ * 型
+ * Int...符号付き32ビット整数
+ * String
+ * Boolean
+ * ID
+ */
+const typeDefs = `
+  type Query {
+    totalPhotos: Int!,
+    isDone: Boolean,
+    allPhotos: [Photo]!,
+  }
+
+  type Mutation {
+    postPhoto(name:String! description:String):Boolean!
+  }
+
+  type Photo {
+    id:ID!,
+    name: String!,
+    url: String!,
+    description: String,
+  }
+  type Fruit {
+    id:ID!,
+    name: String!,
+    color: String!,
+    description: String,
+    ate: Boolean,
+  }
+
+`;
+
+let _id = 0;
+const photos = [
+  {
+    id: 99,
+    name: 'sample',
+    url: 'aaaa',
+    description: 'bbb',
+  },
+];
+
+// typeDefsとresolversと一致するように
+const resolvers = {
+  Query: {
+    totalPhotos: () => photos.length,
+    isDone: () => false,
+    allPhotos: () => photos,
+    // fruits: () => fruits,
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      // 処理を書く
+      let newPhoto = { id: _id++, ...args };
+      photos.push(newPhoto);
+      // データベースに保存する処理もここに
+      return true;
+    }
+  }
+};
+
+
+const fruits = [
+  { name: 'apple', color: 'red' },
+  { name: 'banana', color: 'yellow' },
+  { name: 'cherry', color: 'red' },
+];
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
 
-app.use('/hello', graphqlHTTP({
-  // schema: schema,
-  graphiql: true
-}));
-
-app.listen(4000, () => console.log('Server Running!! port:4000'));
+server.listen(4000).then(() => console.log('4000だよ'));;
